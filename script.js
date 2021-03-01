982e21dd4e0f92bff32bd60d48c7f0070084f323
Mike <mcmiller905@gmail.com>
2021-02-28 21:44:53 -0500

    anchor points

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 document.getElementById("btn").addEventListener("click", displayDate);
-
 function displayDate() {
   document.getElementById("demo").innerHTML = Date();
+}
+
+document.getElementById("snanchor-link").addEventListener("click", goToSnake);
+function goToSnake() {
+    document.location.hash = "";
+    document.location.hash = "#snanchor";
+}
+
+document.getElementById("c-shoot-anchor-link").addEventListener("click", goToShooter);
+function goToShooter() {
+    document.location.hash = "";
+    document.location.hash = "#c-shoot-anchor";
 }
